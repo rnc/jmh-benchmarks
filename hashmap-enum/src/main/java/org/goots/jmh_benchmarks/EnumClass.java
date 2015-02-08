6f8b5423251d923a30b54456ab9334facb9907b5
Nick Cross <github@goots.org>
2015-02-08 23:13:17 +0000

    Initial switch-enum benchmark

---
+++ hashmap-enum/src/main/java/org/goots/jmh_benchmarks/EnumClass.java
@@ -0,0 +1,56 @@
+package org.goots.jmh_benchmarks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Nick Cross on 08/02/15.
+ */
+public class EnumClass
+{
+
+    /**
+     * Enumeration to represent special operations which are handled by stubs
+     */
+    static enum SpecialOps
+    {
+        IS_A("_is_a"),
+        /**
+         * See org.jacorb.orb.Delegate.get_interface_def
+         */
+        INTERFACE("_interface"),
+        GET_POLICY("_get_policy"),
+        NON_EXISTENT("_non_existent"),
+        GET_COMPONENT("_get_component"),
+        REPO_ID("_repository_id"),
+        SET_POLICY("_set_policy_overrides");
+
+        private final String operation;
+
+        private static final Map<String, SpecialOps> map = new HashMap<>(8);
+
+        static SpecialOps getSpecialOperation (String cf)
+        {
+            return map.get(cf);
+        }
+
+        SpecialOps(String operation)
+        {
+            this.operation = operation;
+        }
+
+        public String toString()
+        {
+            return operation;
+        }
+    }
+
+    static
+    {
+        for (SpecialOps type : SpecialOps.values())
+        {
+            SpecialOps.map.put(type.operation, type);
+        }
+    }
+
+}
---
+++ hashmap-enum/src/main/java/org/goots/jmh_benchmarks/EnumSwitchBenchmark.java
@@ -0,0 +1,63 @@
+package org.goots.jmh_benchmarks;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 10)
+@Measurement (iterations = 10)
+public class EnumSwitchBenchmark
+{
+    @Param({"_non_existent", "_repository_id", "_get_policy", "operation"})
+    public String value;
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public boolean testEnum()
+    {
+        return EnumClass.SpecialOps.getSpecialOperation(value) != null;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public boolean testSwitch()
+    {
+
+        boolean result = false;
+
+        switch (value)
+        {
+            case "_get_component":
+            case "_repository_id":
+            {
+                result = true;
+                break;
+            }
+            case "_interface":
+            case "_non_existent":
+            case "_set_policy_overrides":
+            {
+                result = true;
+                break;
+            }
+            case "_get_policy":
+            {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
